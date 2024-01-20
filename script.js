5747777fbe6ea7876084d7eecfea33cff573ac74
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-01-20 15:14:03 -0600

    Write append fucnction inside of linkedList

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+// Node factory with value and nextNode pointer, both set to null by default
+function node(value, nextNode) {
+  if (!value) {
+    value = null;
+  }
+  if (!nextNode) {
+    nextNode = null;
+  }
+  return { value, nextNode };
+}
+
 // LinkedList factory
 function linkedList() {
+  let rootNode = node();
   // append(value)
-  const append = function(value) {
-    
-  }
+  const append = function (value, currentNode) {
+    if (!currentNode) {
+      append(value, rootNode);
+    }
+    if (currentNode.value === null) {
+      currentNode = node(value);
+    } else if (currentNode.nextNode === null) {
+      currentNode.nextNode = node(value);
+    }
+  };
   // prepend(value)
   // size - returns list length
   // head - returns first node in the list
@@ -15,15 +34,6 @@ function linkedList() {
   // toString - return list as a string: ( value ) -> ( value ) -> null
   // insertAt(value, index) - inserts node at index
   // removeAt(index) - removes node at index
-}
 
-// Node factory with value and nextNode pointer, both set to null by default
-function node(value, nextNode) {
-  if (!value) {
-    value = null;
-  }
-  if (!nextNode) {
-    nextNode = null;
-  }
-  return { value, nextNode };
+  return { rootNode, append };
 }
